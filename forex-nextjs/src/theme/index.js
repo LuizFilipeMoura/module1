524a7c101ef08a0db3818e2dc2b397344542cbf6
Luiz Filipe de Moura <50964721+LuizFilipeMoura@users.noreply.github.com>
2021-07-01 18:10:08 -0300

    Update index.js

--- forex-nextjs/src/theme/index.js
+++ forex-nextjs/src/theme/index.js
@@ -10,7 +10,7 @@ const theme = createMuiTheme({
             paper: colors.common.white,
         },
         primary: {
-            main: 'blue',
+            main: '#00FF00',
         },
         secondary: {
             main: '#6d2177',
